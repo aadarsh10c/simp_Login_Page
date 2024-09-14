2aa1ee828a2803ccd0997a7d2e610f496aa02570
adarsh010c <adarsh010c@gmail.com>
2024-09-15 01:03:50 +0530

    tested demo of styled components

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import Test from './test.jsx';
+import StyledComponents from './components/Slider.jsx';
+import Slider from '@mui/material/Slider';
 
 function App() {
     return (
         <>
             <h1>Rick and Morty</h1>
             <Test />
+            <Slider/>
+            <StyledComponents />
         </>
     );
 }
