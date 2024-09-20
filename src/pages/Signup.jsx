769776a6f31a80a33a586dd6d38a97f78b175d97
adarsh010c <adarsh010c@gmail.com>
2024-09-20 16:07:35 +0530

    Basic form created

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { Avatar, Box, Typography, useMediaQuery, Stack } from '@mui/material';
+import {
+    Avatar,
+    Box,
+    Typography,
+    useMediaQuery,
+    Stack,
+    FormControl,
+    InputLabel,
+    OutlinedInput,
+    Grid2 as Grid,
+    Button,
+} from '@mui/material';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
+import BeenhereIcon from '@mui/icons-material/Beenhere';
 import styled, { css } from 'styled-components';
 import { useTheme } from '@mui/material/styles';
+// import { Grid2  } from '@mui/material/Grid2';
 
 const AvatarStyled = styled(Avatar)`
     background-color: ${(props) => props.theme.palette.secondary.main};
@@ -17,12 +30,24 @@ const AvatarStyled = styled(Avatar)`
 
 const SubHeading = styled(Typography)`
     color: ${(props) => props.theme.palette.secondary.main};
-    text-align:center;
+    text-align: center;
 `;
 
 const StackStyled = styled(Stack)`
-      padding: 1.25rem;
-`
+    padding: 1.25rem;
+`;
+
+const FormStyled = styled(Grid)`
+    max-width: 400px;
+    margin: 0 auto;
+    & .Mui-focused.MuiInputBase-formControl {
+        border-color: coral;
+    }
+`;
+
+const ButtonStyled = styled(Button)`
+    margin: 0 auto;
+`;
 
 function Signup() {
     const theme = useTheme();
@@ -40,11 +65,56 @@ function Signup() {
                 </AvatarStyled>
                 <SubHeading variant={match ? 'h6' : 'h5'}>Sign up !</SubHeading>
             </StackStyled>
-            <Box>
-                <Box component="form">
-                    <Typography>This the form</Typography>
-                </Box>
-            </Box>
+            <FormStyled container component="form" spacing={2}>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                    <FormControl size='small' fullWidth required variant="outlined">
+                        <InputLabel htmlFor="firstName-outlined">
+                            First Name
+                        </InputLabel>
+                        <OutlinedInput
+                            id="firstName-outlined"
+                            label="First Name"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid size={{ xs: 12, sm: 6 }}>
+                    <FormControl size='small' fullWidth required variant="outlined">
+                        <InputLabel htmlFor="lastName-outlined">
+                            Last Name
+                        </InputLabel>
+                        <OutlinedInput
+                            id="lastName-outlined"
+                            label="Last Name"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid size={12}>
+                    <FormControl size='small' fullWidth required variant="outlined">
+                        <InputLabel htmlFor="email-outlined">Email</InputLabel>
+                        <OutlinedInput id="firstName-outlined" label="Email" />
+                    </FormControl>
+                </Grid>
+                <Grid size={12}>
+                    <FormControl size='small' fullWidth required variant="outlined">
+                        <InputLabel htmlFor="password-outlined">
+                            Password
+                        </InputLabel>
+                        <OutlinedInput
+                            type="password"
+                            id="password-outlined"
+                            label="Password"
+                        />
+                    </FormControl>
+                </Grid>
+                <Grid size={12} sx={{ textAlign: 'center' }}>
+                    <ButtonStyled
+                        variant="contained"
+                        endIcon={<BeenhereIcon />}
+                    >
+                        Sumit
+                    </ButtonStyled>
+                </Grid>
+            </FormStyled>
         </Box>
     );
 }
